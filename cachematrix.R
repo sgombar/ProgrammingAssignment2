cb69add1009c119b909d2f9625eac9fc2e85610b
sgombar <s.gombar@gmail.com>
2016-06-18 22:07:26 -0700

    Updated for programming class

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Goal: Make a class that can store the inverse of a matrix and return it to prevent long re-compute times
+## 2 functions: one to wrap the matrix and its inverse; (set and get); the other to compute the inverse or pull a previously computed one
 
-## Write a short comment describing this function
+## This funciton creates this matrix wrapper and provides set and get functions for the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	
+	get <- function() x
+	
+	setinv <- function(inverse) inv <<- inverse 
+	
+	getinv <- function() inv
+	
+	list(set = set, get = get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse of this matrix exists; if so it returns it. else it calculates and stores it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inv <- x$getinv()
+		
+		if(!is.null(inv)) {
+			message("Getting Cached Inverse")
+			return(inv)
+		}
+		
+		data <- x$get()
+		inv <- solve(data, ...)
+		x$setinv(inv)
+		inv
 }
